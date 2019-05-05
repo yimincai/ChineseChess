f1c5069b4a75e0ea8fe67bce9d135200eb3c77c1
Neil <bravc29229@gmail.com>
2019-05-06 01:22:16 +0800

    assets add a bg pic

---
+++ Assets/Script/StartBtnOnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartBtnOnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       Button btn = this.GetComponent<Button>(); 
+       btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnClick(){
+        Debug.Log("Btn Clicked. !!");
+    }
+}
