7668615c1d6de7fb2df285a852763a6b5b82c82f
yimincai <bravc29229@gmail.com>
2019-05-08 15:11:04 +0800

    add two btn at mainPage and a panel at gammingPage

---
+++ Assets/Script/QuitGameBtn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuitGameBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnClick()
+    {
+        Debug.Log("QuitGameBtn Clicked!!");
+        // this only works in the compiled game (.exe)
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/StartBtn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnClick()
+    {
+        Debug.Log("StartBtn Clicked!!");
+        SceneManager.LoadScene("GammingScene");
+    }
+}
