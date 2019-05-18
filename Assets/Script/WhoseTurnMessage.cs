d333f985783c3fb8ccf0e4d28860f436eaf99ac5
Neil <bravc29229@gmail.com>
2019-05-18 19:22:33 +0800

    fix wrong project name

--- Assets/Script/WhoseTurnMessage.cs
+++ Assets/Script/WhoseTurnMessage.cs
@@ -18,12 +18,12 @@ public class WhoseTurnMessage : MonoBehaviour
         if (pos._teamFlag)
         {
             team = "黑";
+            this.gameObject.GetComponent<Text>().text = "<color=#000000>換" + team + "方陣營</color>";
         }
         else
         {
             team = "紅";
+            this.gameObject.GetComponent<Text>().text = "<color=#FF2D00>換" + team + "方陣營</color>";
         }
-
-        this.gameObject.GetComponent<Text>().text = "換" + team + "方陣營";
     }
 }
