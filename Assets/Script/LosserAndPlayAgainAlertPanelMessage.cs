88122a5a229f38bd2853ac16b90fd0dc6d4770ed
Neil <bravc29229@gmail.com>
2019-05-19 18:21:05 +0800

    fix wrong script name

---
+++ Assets/Script/LosserAndPlayAgainAlertPanelMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LosserAndPlayAgainAlertPanelMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string team = null;
+        if (pos._teamFlag)
+        {
+            team = "黑";
+        }
+        else
+        {
+            team = "紅";
+        }
+
+        this.gameObject.GetComponent<Text>().text = team + "方投降輸了\n是否再來一局?";
+    }
+}
