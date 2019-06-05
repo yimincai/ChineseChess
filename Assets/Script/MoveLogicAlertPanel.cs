81b05e0529ee0c2ab115ab6d26107777c35dd8eb
yimincai <bravc29229@gmail.com>
2019-06-05 17:29:45 +0800

    delete RedFirstAlertPanel, add bg_GL, add MoveLogicAlertPanel and content

---
+++ Assets/Script/MoveLogicAlertPanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveLogicAlertPanel : MonoBehaviour
+{
+    public Image _moveLogicAlertPanel;
+    public Text _whoseTurnMessage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HideRedFirstAlertOnClick()
+    {
+        _moveLogicAlertPanel.gameObject.SetActive(false);
+        _whoseTurnMessage.gameObject.SetActive(true);
+    }
+}
